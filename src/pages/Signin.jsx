d39dad5223eedd14ac8583c04fade98970c3013f
Teten Nugraha <teten.nugraha18@gmail.com>
2022-04-15 06:13:41 +0700

    cek login

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,29 +1,76 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
-function Signin() {
+const Signin = () => {
+
+  // state
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // redirect
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async(e) => {
+    e.preventDefault();
+
+    const payload = {
+      email,
+      password
+    };
+
+    try {
+      let response = await axios.post('http://localhost:5000/api/v1/signIn', payload);
+      
+      localStorage.setItem("jwt", response.data.token)
+      
+      setRedirect(true);
+
+    }catch(error) {
+      let errorMsg = error.response.data.message;
+      alert(errorMsg);
+    }
+  }
+
+  if(redirect) {
+    return <Navigate to={'/todos'} />
+  }
+
   return (
-    <form class="form-signin">
+    <form class="form-signin" onSubmit={submit}>
         <div class="text-center mb-4">
-            <img class="mb-4" src="/docs/4.6/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
             <h1 class="h3 mb-3 font-weight-normal">Go Productive App</h1>
         </div>
 
         <div class="form-label-group">
             <label for="inputEmail">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" />
+            <input 
+              type="email" 
+              id="email" 
+              class="form-control" 
+              placeholder="Email address" 
+              required="" 
+              onChange={e => setEmail(e.target.value)}
+               />
 
         </div>
 
         <div class="form-label-group">
             <label for="inputPassword">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" />
+            <input 
+              type="password" 
+              id="password" 
+              class="form-control" 
+              placeholder="Password" 
+              onChange={e => setPassword(e.target.value)}
+             />
 
         </div>
 
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted text-center">© 2017-2021</p>
+        <p class="mt-5 mb-3 text-muted text-center">© 2022</p>
     </form>
   )
 }
 
-export default Signin
+export default Signin;
